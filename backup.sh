8309674fb3fe0118411495276aa7de12f136d883
shijingyu <945090896@qq.com>
2017-04-02 09:49:56 +0800

    chaifenjiaoben

--- backup.sh
+++ backup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 filename=`date +%Y-%m-%d`"-MkStone.sql"
 mysqldump -uroot -p960722 MkStone > ~/data/MkStone-backupSQL/$filename
-git add $filename
-git commit -m '"auto commit"'
-git push origin master
